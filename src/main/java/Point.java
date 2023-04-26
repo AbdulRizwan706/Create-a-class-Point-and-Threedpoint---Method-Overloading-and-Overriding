eaf384a334a8138771e2f474d7277942d5f84c79
Abdul Rizwan <abdulrizwan706@gmail.com>
2023-04-26 12:44:33 +0530

    this is it

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,3 +1,13 @@
 public class Point {
     // write the code of point class here
+    // i. It should have two data members
+    // x:int
+    // y: int
+    // It should have a display method that prints in following format - “[<<x>>, <<y>>]”
+    // x and y should be protected and display should be public
+    public int x;
+    public int y;
+    public void display(){
+        System.out.println("“[<<" + this.x +">>, <<"+ this.y + ">>]”");
+    }
 }
--- src/main/java/ThreedPoint.java
+++ src/main/java/ThreedPoint.java
@@ -1,3 +1,12 @@
-public class ThreedPoint {
+public class ThreedPoint extends Point{
     // write the code of Threedpoint class here
+    // t should have the following data members: z:int
+    // It should have a display method to override the parent’s
+    // display method which prints in following format - “[<<x>>, <<y>>, <<z>>]”
+    // z should be private and display should be public.
+    int z;
+    Point p = new Point();
+    public void display(){
+        System.out.println("“[<<" + p.x + ">>, <<" + p.y + ">>, <<" + this.z + ">>]”");
+    }
 }
